b48efe46de75b45c6f7a6001c1040823fe6b2cd3
FannyVieira <FannyVieira>
2016-01-22 15:35:35 -0300

    - Initial commit.

--- Aid-Manager/src/br/edu/ifpb/auxilio/dominio/AssistenteSocial.java
+++ Aid-Manager/src/br/edu/ifpb/auxilio/dominio/AssistenteSocial.java
@@ -4,16 +4,21 @@ import java.security.Timestamp;
 
 public class AssistenteSocial extends Servidor {
 	private int id_assistenteSocial;
+
 	
+	public AssistenteSocial(){
+		super();
+       setId_assistenteSocial(0);	
+    }
 	
-public AssistenteSocial(int id_assistenteSocial, int idPessoa,String nomePessoa,
+	public AssistenteSocial(int id_assistenteSocial, int idPessoa,String nomePessoa,
 						String matricula,Timestamp dataNasc,String senha,String email,
 						String cpf,String rg,String sexo,int idServidor,String cargoServidor){
 	
 	super(idPessoa,nomePessoa,matricula,dataNasc,senha,email,cpf,rg,sexo,idServidor,cargoServidor );
 	setId_assistenteSocial(id_assistenteSocial);	
-}
-//-------------------------------------------------------------------------------
+	}
+//------------------------------GETTERS E SETTERS -------------------------------------------------//
 	public int getId_assistenteSocial() {
 		return id_assistenteSocial;
 	}
