34be3bd39dc83fd0b32efa4b0f9154158c7b7581
RaylaMedeiros <Rayla.Medeiiros@gmail.com>
2016-01-22 15:26:17 -0300

    - Initial commit

---
+++ Aid-Manager/src/br/edu/ifpb/auxilio/dominio/Processo.java
@@ -0,0 +1,66 @@
+package br.edu.ifpb.auxilio.dominio;
+
+import java.util.*;
+
+public class Processo {
+	private Date data;
+	private String obs;
+	private String numProcesso;
+	private String assunto;
+	private Discente interessado;
+	
+	// CONSTRUTOR
+	
+	public Processo(Date data, String obs, String numProcesso, String assunto,
+			Discente interessado) {
+		super();
+		this.data = data;
+		this.obs = obs;
+		this.numProcesso = numProcesso;
+		this.assunto = assunto;
+		this.interessado = interessado;
+	}
+	
+	//GETTERS E SETTERS
+	
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getObs() {
+		return obs;
+	}
+
+	public void setObs(String obs) {
+		this.obs = obs;
+	}
+
+	public String getNumProcesso() {
+		return numProcesso;
+	}
+
+	public void setNumProcesso(String numProcesso) {
+		this.numProcesso = numProcesso;
+	}
+
+	public String getAssunto() {
+		return assunto;
+	}
+
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+
+	public Discente getInteressado() {
+		return interessado;
+	}
+
+	public void setInteressado(Discente interessado) {
+		this.interessado = interessado;
+	}
+	
+}
