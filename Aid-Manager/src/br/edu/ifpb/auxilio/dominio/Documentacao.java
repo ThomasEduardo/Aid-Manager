e77e964539d549c1cef93d061c2097921a4be0f1
RaylaMedeiros <Rayla.Medeiiros@gmail.com>
2016-01-22 15:06:14 -0300

    - Initial commit.

---
+++ Aid-Manager/src/br/edu/ifpb/auxilio/dominio/Documentacao.java
@@ -0,0 +1,76 @@
+package br.edu.ifpb.auxilio.dominio;
+
+import java.io.File;
+
+public class Documentacao {
+	private int idDocumentacao;
+	private String nomeDocumentacao;
+	private String status;
+	private File conteudo;
+	private String obs;
+	private Discente discente;
+	
+	// CONSTRUTOR
+	
+	public Documentacao(int idDocumentacao, String nomeDocumentacao,
+			String status, File conteudo, String obs, Discente discente) {
+		super();
+		this.idDocumentacao = idDocumentacao;
+		this.nomeDocumentacao = nomeDocumentacao;
+		this.status = status;
+		this.conteudo = conteudo;
+		this.obs = obs;
+		this.discente = discente;
+	}
+	
+	// GETTERS E SETTERS
+
+	public int getIdDocumentacao() {
+		return idDocumentacao;
+	}
+
+	public void setIdDocumentacao(int idDocumentacao) {
+		this.idDocumentacao = idDocumentacao;
+	}
+
+	public String getNomeDocumentacao() {
+		return nomeDocumentacao;
+	}
+
+	public void setNomeDocumentacao(String nomeDocumentacao) {
+		this.nomeDocumentacao = nomeDocumentacao;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public File getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(File conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public String getObs() {
+		return obs;
+	}
+
+	public void setObs(String obs) {
+		this.obs = obs;
+	}
+
+	public Discente getDiscente() {
+		return discente;
+	}
+
+	public void setDiscente(Discente discente) {
+		this.discente = discente;
+	}
+
+}
