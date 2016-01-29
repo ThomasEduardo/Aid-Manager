48eee14e889a0e2e36b9ac90751d8a6d19f79e94
RaylaMedeiros <Rayla.Medeiiros@gmail.com>
2016-01-29 17:23:36 -0300

    - Initial commit

---
+++ Aid-Manager/src/br/edu/ifpb/auxilio/bd/Conexao.java
@@ -0,0 +1,29 @@
+package br.edu.ifpb.auxilio.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+	
+	public static String status = "";
+
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			// Carregando o driver JDBC para MySQL. Cada SGBD tem seu próprio driver
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			
+			String url = "jdbc:mysql://localhost/banco?user=root&password=password";
+
+			conn = DriverManager.getConnection(url);
+			
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+}
