5d00d1c88d0bc088ab6d8ba2acbdb25f0d1cb1e5
FannyVieira <FannyVieiira@hotmail.com>
2016-02-10 14:11:24 -0200

    - Correção do Script.

--- Aid-Manager/src/br/edu/ifpb/auxilio/recursos/ScriptAManager.sql
+++ Aid-Manager/src/br/edu/ifpb/auxilio/recursos/ScriptAManager.sql
@@ -1,6 +1,6 @@
-  CREATE DATABASE aidmanager;
-  
-  use aidmanager;
+		CREATE DATABASE aidManager;
+		USE aidManager;
+		
 		Create table  pessoa(
 				idPessoa int unsigned auto_increment primary key,
 				nomePessoa varchar(255),
@@ -18,14 +18,14 @@
 				idTelefone int unsigned auto_increment primary key, 
 				telefoneResidencial varchar(50), 
 				telefoneCelular varchar(50), 
-				idPessoa int unsigned,
+				idPessoa int unsigned not null,
 				constraint fk_telefone_pessoa foreign key(idPessoa) references pessoa(idPessoa) 
 		);
 	
 		Create table servidor(
 				  idServidor int unsigned auto_increment primary key,
 				  cargoServidor varchar(30),
-				  idPessoa int unsigned,
+				  idPessoa int unsigned not null,
 				  constraint fk_servidor_pessoa foreign key(idPessoa) references pessoa(idPessoa)
 		);
 	
@@ -33,7 +33,7 @@
 		Create table assistenteSocial(
     
 				idAssistenteSocial int unsigned auto_increment primary key,
-				idServidor int unsigned,
+				idServidor int unsigned not null,
 				constraint fk_assistenteSocial_Servidor foreign key(idServidor) references servidor(idServidor)
     
 		);
@@ -41,7 +41,7 @@
 		Create table TecnicoAdmin(
     
 				idTecnicoAdmin int unsigned auto_increment primary key,
-				idServidor int unsigned,
+				idServidor int unsigned not null,
 				constraint fk_tecnicoAdmin_Servidor foreign key(idServidor) references servidor(idServidor)
 		);
 	
@@ -51,7 +51,7 @@
 				nomeIF varchar(40), 
 				cnpj varchar(40), 
 				orcamentoAuxilio double, 
-				idTecnicoAdmin int unsigned, 
+				idTecnicoAdmin int unsigned not null, 
 				constraint fk_instituicaFinanciado_tecnicoAdmin foreign key(idTecnicoAdmin) references tecnicoAdmin(idTecnicoAdmin)
 				
 		);
@@ -65,108 +65,18 @@
 				numProcesso varchar(50),
 				assunto varchar(255),
 				parecer varchar(255),
-				idInteressado int unsigned,
-				idServidor int unsigned,
+				idInteressado int unsigned not null,
+				idServidor int unsigned not null,
 				constraint fk_processo_interessado foreign key(idInteressado) references pessoa(idPessoa),
 				constraint fk_processo_servidor foreign key(idServidor) references servidor(idServidor)
 				
 		);
-		Create table auxilio(
-    
-				idAuxilio int unsigned auto_increment primary key,
-				tipoAuxilio varchar(40),
-				valorAuxilio double,
-				validadeInicial date,
-				validadeFinal date,
-				idInstituicaoFinanciadora int unsigned,
-				idTecnicoAdmin int unsigned,
-    			idProcesso int unsigned,
-				constraint fk_auxilio_IF foreign key(idInstituicaoFinanciadora) references instituicaoFinanciadora(idIF),
-				constraint fk_auxilio_TecnicoAdmin foreign key(idTecnicoAdmin) references tecnicoAdmin(idTecnicoAdmin),
-    			constraint fk_auxilio_processo foreign key(idProcesso) references processo(idProcesso)
-
-		);
- 
-		Create table edital(
-		
-				idEdital int unsigned auto_increment primary key,
-				iniInscricoes date,
-				FimInscricoes date,
-				iniEntregaForm date ,
-				ano int,
-				fimForm date,
-				descricao varchar(255),
-				titulo varchar(100),
-				valorBolsaDiscente double,
-				vagasBolsistas int,
-				numEdital varchar(50),
-				idProcesso int unsigned,
-				constraint fk_edital_processo foreign key(idProcesso) references processo(idProcesso) 
-		
-		);
-	
-		Create table dadosBancarios(
-		
-				idDadosBancarios int unsigned auto_increment primary key,
-				banco varchar(255),
-				agencia varchar(255),
-				numAgencia varchar(255)
-		
-		);
-		
 		create table resultados ( 
 				idResultados int unsigned auto_increment primary key, 
-				tipoauxilio VARCHAR(30)
+				tipoauxilio VARCHAR(30),
+				idProcesso int unsigned not null,
+				constraint fk_resultados_processo foreign key(idProcesso) references processo(idProcesso)
 		);
-
-
-		Create table PerfilSocioEconomico(
-		    
-		    
-				idPerfilSocio int unsigned auto_increment primary key,
-				situacaoRendaFamiliar varchar(255),
-				moradia varchar(255),
-				situacaoMoradia varchar(255),
-				residenciaFamiliar varchar(255),
-				situacaoTrabalho varchar(255),
-				aluguel double,
-				condominio double,
-				luz double,
-				agua double,
-				telefone double,
-				financiamentoCasaPropria double,
-                idAssistenteSocial int unsigned,
-                constraint fk_perfilsocioeconomico_Assistentesocial foreign key(idAssistenteSocial) references assistenteSocial(idAssistenteSocial)
-		    
-		);
-		
-		Create table situacaoSaude(
-		    
-		    idSituacaoSaude int unsigned auto_increment primary key,
-			membro varchar(50),
-			doenca varchar(50),
-			idPerfilSocioEconomico int unsigned,
-		    constraint fk_situacaoSaude_perfilSocio foreign key (idPerfilSocioEconomico) references perfilSocioEconomico(idPerfilSocio)
-		);
-		Create table residentesMoradia(
-			    idRm int unsigned,
-				residentes varchar(255),
-				idPerfilSocio int unsigned,
-			    constraint fk_residentesMoradia_perfilSocio foreign key(idPerfilSocio) references perfilSocioEconomico(idPerfilSocio)
-			    
-		);
-
-		Create table composicaoRendaFamiliar( 
-			idCrf int unsigned auto_increment primary key, 
-			nome varchar(70), 
-			idade int unsigned, 
-			grauDeInstrucao int unsigned, 
-			profissao varchar(70), 
-			renda double,
-			idPerfilSocio int unsigned, 
-			constraint fk_composicaoRendaFamiliar_idPerfilSocio foreign key(idPerfilSocio) references perfilSocioEconomico(idPerfilSocio) 
-		);
-	
 		Create table Discente ( 
 
 				idDiscente int unsigned auto_increment primary key, 
@@ -185,23 +95,115 @@
 				numCasa int,
 				pontoRef varchar(50),
 				estado varchar(60),
-				motivoSolicitacao varchar(255), 
-				idAux int unsigned, 
-				idDb int unsigned , 
-				idperfilSocio int unsigned, 
-				idResultados int unsigned,
-				constraint fk_discente_Auxilio foreign key(idAux) references auxilio(idAuxilio), 
-				constraint fk_discente_DadosBancarios foreign key(idDb) references dadosBancarios(idDadosBancarios),
-				constraint fk_discente_PerfilSocio foreign key(idperfilSocio) references perfilSocioEconomico(idPerfilSocio),
+				motivoSolicitacao varchar(255),
+				idPessoa int unsigned not null,
+				idResultados int unsigned not null,
+				constraint fk_discente_pessoa foreign key (idPessoa) references pessoa(idPessoa),
 				constraint fk_discente_resultados foreign key (idResultados) references Resultados(idResultados)
 		);
+		
+		
+		Create table auxilio(
+    
+				idAuxilio int unsigned auto_increment primary key,
+				tipoAuxilio varchar(40),
+				valorAuxilio double,
+				validadeInicial date,
+				validadeFinal date,
+				idInstituicaoFinanciadora int unsigned,
+				idTecnicoAdmin int unsigned,
+    			idProcesso int unsigned not null,
+                idDiscente int unsigned not null,
+				constraint fk_auxilio_IF foreign key(idInstituicaoFinanciadora) references instituicaoFinanciadora(idIF),
+				constraint fk_auxilio_TecnicoAdmin foreign key(idTecnicoAdmin) references tecnicoAdmin(idTecnicoAdmin),
+    			constraint fk_auxilio_processo foreign key(idProcesso) references processo(idProcesso),
+                constraint fk_auxilio_Discente foreign key(idDiscente) references discente(idDiscente)
+
+		);
+		Create table edital(
+		
+				idEdital int unsigned auto_increment primary key,
+				iniInscricoes date,
+				FimInscricoes date,
+				iniEntregaForm date ,
+				ano int,
+				fimForm date,
+				descricao varchar(255),
+				titulo varchar(100),
+				valorBolsaDiscente double,
+				vagasBolsistas int,
+				numEdital varchar(50),
+				idProcesso int unsigned not null,
+				constraint fk_edital_processo foreign key(idProcesso) references processo(idProcesso) 
+		
+		);
+		Create table dadosBancarios(
+		
+				idDadosBancarios int unsigned auto_increment primary key,
+				banco varchar(255),
+				agencia varchar(255),
+				numAgencia varchar(255),
+                saldo double,
+                idDiscente int unsigned not null,
+                constraint fk_dadosBancarios_Discente foreign key(idDiscente) references discente(idDiscente)
+		
+		);
+		Create table PerfilSocioEconomico(
+		    
+		    
+				idPerfilSocio int unsigned auto_increment primary key,
+				situacaoRendaFamiliar varchar(255),
+				moradia varchar(255),
+				situacaoMoradia varchar(255),
+				residenciaFamiliar varchar(255),
+				situacaoTrabalho varchar(255),
+				aluguel double,
+				condominio double,
+				luz double,
+				agua double,
+				telefone double,
+				financiamentoCasaPropria double,
+                idAssistenteSocial int unsigned not null,
+                idDiscente int unsigned not null,
+                constraint fk_perfilsocioeconomico_Assistentesocial foreign key(idAssistenteSocial) references assistenteSocial(idAssistenteSocial),
+                constraint fk_perfilsocioeconomico_Discente foreign key(idDiscente) references discente(idDiscente)
+          
+		);
+		Create table situacaoSaude(
+		    
+		    idSituacaoSaude int unsigned auto_increment primary key,
+			membro varchar(50),
+			doenca varchar(50),
+			idPerfilSocioEconomico int unsigned not null,
+		    constraint fk_situacaoSaude_perfilSocio foreign key (idPerfilSocioEconomico) references perfilSocioEconomico(idPerfilSocio)
+		);
+		Create table residentesMoradia(
+			    idRm int unsigned,
+				residentes varchar(255),
+				idPerfilSocio int unsigned not null,
+			    constraint fk_residentesMoradia_perfilSocio foreign key(idPerfilSocio) references perfilSocioEconomico(idPerfilSocio)
+			    
+		);
+
+		Create table composicaoRendaFamiliar( 
+			idCrf int unsigned auto_increment primary key, 
+			nome varchar(70), 
+			idade int unsigned, 
+			grauDeInstrucao int unsigned, 
+			profissao varchar(70), 
+			renda double,
+			idPerfilSocio int unsigned not null, 
+			constraint fk_composicaoRendaFamiliar_idPerfilSocio foreign key(idPerfilSocio) references perfilSocioEconomico(idPerfilSocio) 
+		);
+		
 		Create table documentacao (
 			
 			idDocumentacao int unsigned auto_increment primary key,
 			nomeDocumentacao varchar(100),
 			status_Documento varchar(255),
 			obs varchar(255),
-			iddiscente int unsigned,
+			idDiscente int unsigned not null,
 			constraint fk_documentacao_discente foreign key(idDiscente) references discente(idDiscente)
 
 		);
+		
