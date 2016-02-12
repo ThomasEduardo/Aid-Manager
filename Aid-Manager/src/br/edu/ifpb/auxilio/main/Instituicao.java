fe0bd9b046cf1317bd631aecece799ae9abdab2d
Fanny <fannyvieiira@hotmail.com>
2016-02-12 17:45:01 -0300

    - Updates that need to be corrected

--- Aid-Manager/src/br/edu/ifpb/auxilio/main/Instituicao.java
+++ Aid-Manager/src/br/edu/ifpb/auxilio/main/Instituicao.java
@@ -9,11 +9,9 @@ public class Instituicao {
 public static void main(String[] args) {
 	
 	
-	ServidorDAO s = new ServidorDAO();
-	PessoaDAO pessoa = new PessoaDAO();
-	AssistenteSocial a = new AssistenteSocial("Rayla","20141004029",null,"123","rayla.medeiiros@gmail.com","121.333.111-22","4.999.259","feminino","assistente social");
-	pessoa.Cadastrar(a);
-	s.Cadastrar(a);
+	ServidorDAO p = new ServidorDAO();
+	p.remover("20141004029");
+	
 	
 	
 	
